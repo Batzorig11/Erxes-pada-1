652c2b2b6383cfed5d8f1e8909ca7a0c943b8786
Batzorig <lbatzorig11@gmail.com>
2025-10-15 12:49:52 +0800

    weather app

--- weather/main.js
+++ weather/main.js
@@ -1,20 +1,15 @@
 const main = async () => {
-  const input = document.getElementById("input").value;
-  const button = document.getElementById("button");
-  const result1 = document.getElementById("result1");
-  const result2 = document.getElementById("result2");
-  const result3 = document.getElementById("result3");
-  const result4 = document.getElementById("result4");
-
+  const city = document.getElementById("input").value;
+  const results = document.getElementById("results");
   const response = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=ffc74c9e41d3cd99bf6bb25a7f582a7a`
   );
   const data = await response.json();
   console.log(data);
-  result1.innerHTML = `Temperature : ${data.main.temp} &#176;C `;
-  result2.innerHTML = `Weather : ${data.weather[0].main} `;
-  result3.innerHTML = `Humidity : ${data.main.humidity}% `;
-  result4.innerHTML = `Wind Speed : ${data.wind.speed}m/s`;
+  results.innerHTML = `Weather : ${data.weather[0].main}
+  Temperature : ${data.main.temp} &#176;C
+  Wind Speed : ${data.wind.speed}m/s`;
+  console.log(city);
 };
 
 main();
